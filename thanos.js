0b2d101f8836b1453407aec169603c4498bd6170
Krishna Bhamidipati <NaanProphet@users.noreply.github.com>
2020-03-27 12:25:01 -0400

    variable names and elseif statements
    - making the code easier to read/modify
    - no functional changes

--- thanos.js
+++ thanos.js
@@ -9,230 +9,185 @@ var SecondNoteOff = new NoteOff;
 var DelayCompensator;
 var LastKnownModwheel;
 
-function HandleMIDI(_0xb441xa) {
-    StoreModwheel(_0xb441xa);
-    if (_0xb441xa['articulationID'] == 2) {
-        if (_0xb441xa instanceof NoteOn && _0xb441xa['velocity'] <= 64) {
-            Relay(_0xb441xa, 300, 10)
+function HandleMIDI(event) {
+    StoreModwheel(event);
+    if (event['articulationID'] == 2) {
+        if (event instanceof NoteOn && event['velocity'] <= 64) {
+            Relay(event, 300, 10)
+        } else if (event instanceof NoteOn && event['velocity'] >= 65 && event['velocity'] <= 100) {
+            Relay(event, 215, 10)
+        } else if (event instanceof NoteOn && event['velocity'] >= 101) {
+            Relay(event, 120, 10)
+        } else if (event instanceof NoteOff) {
+            Relay(event, 150, 10)
         } else {
-            if (_0xb441xa instanceof NoteOn && _0xb441xa['velocity'] >= 65 && _0xb441xa['velocity'] <= 100) {
-                Relay(_0xb441xa, 215, 10)
-            } else {
-                if (_0xb441xa instanceof NoteOn && _0xb441xa['velocity'] >= 101) {
-                    Relay(_0xb441xa, 120, 10)
-                } else {
-                    if (_0xb441xa instanceof NoteOff) {
-                        Relay(_0xb441xa, 150, 10)
-                    } else {
-                        Relay(_0xb441xa, 0, 10)
-                    }
-                }
-            }
+            Relay(event, 0, 10)
         }
+    } else if (event['articulationID'] == 3) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 10, 10)
+        } else {
+            Relay(event, 0, 10)
+        }
+    } else if (event['articulationID'] == 12) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 60, 27)
+        } else {
+            Relay(event, 0, 27)
+        }
+    } else if (event['articulationID'] == 13) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 60, 24)
+        } else {
+            Relay(event, 0, 24)
+        }
+    } else if (event['articulationID'] == 14) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 60, 18)
+        } else {
+            Relay(event, 0, 18)
+        }
+    } else if (event['articulationID'] == 15) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 60, 14)
+        } else {
+            Relay(event, 0, 14)
+        }
+    } else if (event['articulationID'] == 16) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 60, 33)
+        } else {
+            Relay(event, 0, 33)
+        }
+    } else if (event['articulationID'] == 17) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 60, 37)
+        } else {
+            Relay(event, 0, 37)
+        }
+    } else if (event['articulationID'] == 18) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 60, 44)
+        } else {
+            Relay(event, 0, 44)
+        }
+    } else if (event['articulationID'] == 19) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 10, 53)
+        } else {
+            Relay(event, 0, 53)
+        }
+    } else if (event['articulationID'] == 20) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 10, 57)
+        } else {
+            Relay(event, 0, 57)
+        }
+    } else if (event['articulationID'] == 21) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 10, 63)
+        } else {
+            Relay(event, 0, 63)
+        }
+    } else if (event['articulationID'] == 22) {
+        if (event instanceof NoteOn) {
+            TrillRelayOn(event, 10, 1)
+        } else if (event instanceof NoteOff) {
+            TrillRelayOff(event, 10, 1)
+        } else {
+            Relay(event, 0, 48)
+        }
+    } else if (event['articulationID'] == 23) {
+        if (event instanceof NoteOn) {
+            TrillRelayOn(event, 10, 2)
+        } else if (event instanceof NoteOff) {
+            TrillRelayOff(event, 10, 2)
+        } else {
+            Relay(event, 0, 48)
+        }
+    } else if (event['articulationID'] == 4) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 50, 67)
+        } else {
+            Relay(event, 0, 67)
+        }
+    } else if (event['articulationID'] == 5) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 10, 67)
+        } else {
+            Relay(event, 0, 67)
+        }
+    } else if (event['articulationID'] == 26) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 50, 73)
+        } else {
+            Relay(event, 0, 73)
+        }
+    } else if (event['articulationID'] == 27) {
+        if (event instanceof NoteOn || event instanceof NoteOff) {
+            Relay(event, 10, 73)
+        } else {
+            Relay(event, 0, 73)
+        }
+    } else if (event instanceof NoteOn || event instanceof NoteOff) {
     } else {
-        if (_0xb441xa['articulationID'] == 3) {
-            if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                Relay(_0xb441xa, 10, 10)
-            } else {
-                Relay(_0xb441xa, 0, 10)
-            }
-        } else {
-            if (_0xb441xa['articulationID'] == 12) {
-                if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                    Relay(_0xb441xa, 60, 27)
-                } else {
-                    Relay(_0xb441xa, 0, 27)
-                }
-            } else {
-                if (_0xb441xa['articulationID'] == 13) {
-                    if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                        Relay(_0xb441xa, 60, 24)
-                    } else {
-                        Relay(_0xb441xa, 0, 24)
-                    }
-                } else {
-                    if (_0xb441xa['articulationID'] == 14) {
-                        if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                            Relay(_0xb441xa, 60, 18)
-                        } else {
-                            Relay(_0xb441xa, 0, 18)
-                        }
-                    } else {
-                        if (_0xb441xa['articulationID'] == 15) {
-                            if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                                Relay(_0xb441xa, 60, 14)
-                            } else {
-                                Relay(_0xb441xa, 0, 14)
-                            }
-                        } else {
-                            if (_0xb441xa['articulationID'] == 16) {
-                                if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                                    Relay(_0xb441xa, 60, 33)
-                                } else {
-                                    Relay(_0xb441xa, 0, 33)
-                                }
-                            } else {
-                                if (_0xb441xa['articulationID'] == 17) {
-                                    if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                                        Relay(_0xb441xa, 60, 37)
-                                    } else {
-                                        Relay(_0xb441xa, 0, 37)
-                                    }
-                                } else {
-                                    if (_0xb441xa['articulationID'] == 18) {
-                                        if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                                            Relay(_0xb441xa, 60, 44)
-                                        } else {
-                                            Relay(_0xb441xa, 0, 44)
-                                        }
-                                    } else {
-                                        if (_0xb441xa['articulationID'] == 19) {
-                                            if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                                                Relay(_0xb441xa, 10, 53)
-                                            } else {
-                                                Relay(_0xb441xa, 0, 53)
-                                            }
-                                        } else {
-                                            if (_0xb441xa['articulationID'] == 20) {
-                                                if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                                                    Relay(_0xb441xa, 10, 57)
-                                                } else {
-                                                    Relay(_0xb441xa, 0, 57)
-                                                }
-                                            } else {
-                                                if (_0xb441xa['articulationID'] == 21) {
-                                                    if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                                                        Relay(_0xb441xa, 10, 63)
-                                                    } else {
-                                                        Relay(_0xb441xa, 0, 63)
-                                                    }
-                                                } else {
-                                                    if (_0xb441xa['articulationID'] == 22) {
-                                                        if (_0xb441xa instanceof NoteOn) {
-                                                            TrillRelayOn(_0xb441xa, 10, 1)
-                                                        } else {
-                                                            if (_0xb441xa instanceof NoteOff) {
-                                                                TrillRelayOff(_0xb441xa, 10, 1)
-                                                            } else {
-                                                                Relay(_0xb441xa, 0, 48)
-                                                            }
-                                                        }
-                                                    } else {
-                                                        if (_0xb441xa['articulationID'] == 23) {
-                                                            if (_0xb441xa instanceof NoteOn) {
-                                                                TrillRelayOn(_0xb441xa, 10, 2)
-                                                            } else {
-                                                                if (_0xb441xa instanceof NoteOff) {
-                                                                    TrillRelayOff(_0xb441xa, 10, 2)
-                                                                } else {
-                                                                    Relay(_0xb441xa, 0, 48)
-                                                                }
-                                                            }
-                                                        } else {
-                                                            if (_0xb441xa['articulationID'] == 4) {
-                                                                if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                                                                    Relay(_0xb441xa, 50, 67)
-                                                                } else {
-                                                                    Relay(_0xb441xa, 0, 67)
-                                                                }
-                                                            } else {
-                                                                if (_0xb441xa['articulationID'] == 5) {
-                                                                    if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                                                                        Relay(_0xb441xa, 10, 67)
-                                                                    } else {
-                                                                        Relay(_0xb441xa, 0, 67)
-                                                                    }
-                                                                } else {
-                                                                    if (_0xb441xa['articulationID'] == 26) {
-                                                                        if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                                                                            Relay(_0xb441xa, 50, 73)
-                                                                        } else {
-                                                                            Relay(_0xb441xa, 0, 73)
-                                                                        }
-                                                                    } else {
-                                                                        if (_0xb441xa['articulationID'] == 27) {
-                                                                            if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {
-                                                                                Relay(_0xb441xa, 10, 73)
-                                                                            } else {
-                                                                                Relay(_0xb441xa, 0, 73)
-                                                                            }
-                                                                        } else {
-                                                                            if (_0xb441xa instanceof NoteOn || _0xb441xa instanceof NoteOff) {} else {
-                                                                                Relay(_0xb441xa, 0, 0)
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        Relay(event, 0, 0)
     }
 }
 
-function Relay(_0xb441xa, _0xb441xc, _0xb441xd) {
-    DelayCompensator = 500 - _0xb441xc;
-    if (_0xb441xd > 0) {
-        CSSArt['value'] = _0xb441xd;
-        CSSArt['channel'] = _0xb441xa['channel'];
+function Relay(event, offset, cc58KeySwitch) {
+    DelayCompensator = 500 - offset;
+    if (cc58KeySwitch > 0) {
+        CSSArt['value'] = cc58KeySwitch;
+        CSSArt['channel'] = event['channel'];
         CSSArt['sendAfterMilliseconds'](DelayCompensator)
     };
-    _0xb441xa['sendAfterMilliseconds'](DelayCompensator);
-    RestoreModwheel(_0xb441xa)
+    event['sendAfterMilliseconds'](DelayCompensator);
+    RestoreModwheel(event)
 }
 
-function StoreModwheel(_0xb441xa) {
-    if (_0xb441xa instanceof ControlChange && _0xb441xa['number'] == 1) {
-        LastKnownModwheel = _0xb441xa['value']
+function StoreModwheel(event) {
+    if (event instanceof ControlChange && event['number'] == 1) {
+        LastKnownModwheel = event['value']
     }
 }
 
-function RestoreModwheel(_0xb441xa) {
-    if (_0xb441xa['articulationID'] == 2 || 3 || 19 || 20 || 21 || 22 || 4 || 5 || 22 || 23 || 26 || 27) {
+function RestoreModwheel(event) {
+    if (event['articulationID'] == 2 || 3 || 19 || 20 || 21 || 22 || 4 || 5 || 22 || 23 || 26 || 27) {
         RestoredModwheel['value'] = LastKnownModwheel;
-        RestoredModwheel['channel'] = _0xb441xa['channel'];
+        RestoredModwheel['channel'] = event['channel'];
         RestoredModwheel['sendAfterMilliseconds'](DelayCompensator)
     }
 }
 
-function TrillRelayOn(_0xb441xa, _0xb441xc, _0xb441x11) {
-    DelayCompensator = 500 - _0xb441xc;
+function TrillRelayOn(event, offset, halfSteps) {
+    DelayCompensator = 500 - offset;
     CSSArt['value'] = 48;
-    CSSArt['channel'] = _0xb441xa['channel'];
+    CSSArt['channel'] = event['channel'];
     CSSArt['sendAfterMilliseconds'](DelayCompensator);
-    NewOn['channel'] = _0xb441xa['channel'];
-    NewOn['velocity'] = _0xb441xa['velocity'];
-    SecondNoteOn['channel'] = _0xb441xa['channel'];
-    SecondNoteOn['velocity'] = _0xb441xa['velocity'];
-    NewOn['pitch'] = _0xb441xa['pitch'];
-    SecondNoteOn['pitch'] = _0xb441xa['pitch'] + _0xb441x11;
+    NewOn['channel'] = event['channel'];
+    NewOn['velocity'] = event['velocity'];
+    SecondNoteOn['channel'] = event['channel'];
+    SecondNoteOn['velocity'] = event['velocity'];
+    NewOn['pitch'] = event['pitch'];
+    SecondNoteOn['pitch'] = event['pitch'] + halfSteps;
     NewOn['sendAfterMilliseconds'](DelayCompensator);
     SecondNoteOn['sendAfterMilliseconds'](DelayCompensator);
-    RestoreModwheel(_0xb441xa)
+    RestoreModwheel(event)
 }
 
-function TrillRelayOff(_0xb441xa, _0xb441xc, _0xb441x11) {
-    DelayCompensator = 500 - _0xb441xc;
+function TrillRelayOff(event, offset, halfSteps) {
+    DelayCompensator = 500 - offset;
     CSSArt['value'] = 48;
-    CSSArt['channel'] = _0xb441xa['channel'];
+    CSSArt['channel'] = event['channel'];
     CSSArt['sendAfterMilliseconds'](DelayCompensator);
-    NewOff['channel'] = _0xb441xa['channel'];
-    NewOff['velocity'] = _0xb441xa['velocity'];
-    SecondNoteOff['channel'] = _0xb441xa['channel'];
-    SecondNoteOff['velocity'] = _0xb441xa['velocity'];
-    NewOff['pitch'] = _0xb441xa['pitch'];
-    SecondNoteOff['pitch'] = _0xb441xa['pitch'] + _0xb441x11;
+    NewOff['channel'] = event['channel'];
+    NewOff['velocity'] = event['velocity'];
+    SecondNoteOff['channel'] = event['channel'];
+    SecondNoteOff['velocity'] = event['velocity'];
+    NewOff['pitch'] = event['pitch'];
+    SecondNoteOff['pitch'] = event['pitch'] + halfSteps;
     NewOff['sendAfterMilliseconds'](DelayCompensator);
     SecondNoteOff['sendAfterMilliseconds'](DelayCompensator);
-    RestoreModwheel(_0xb441xa)
+    RestoreModwheel(event)
 }
